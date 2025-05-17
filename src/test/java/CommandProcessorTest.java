0619bed65c8a06d8e1206382871c6e1e002d450d
AlekseiNikitin9 <alexeynikitin090@gmail.com>
2025-05-16 20:53:42 -0400

    Added few more tests for command processor

--- src/test/java/CommandProcessorTest.java
+++ src/test/java/CommandProcessorTest.java
@@ -22,5 +22,30 @@ public class CommandProcessorTest {
         assertEquals(1.0, acc.getApr());
         assertEquals(0, acc.getBalance());
     }
+    @Test
+    public void testCreateCdAccountWithBalance() {
+        processor.process("create cd 87654321 2.5 3000");
+        Account acc = bank.retrieveAccount(87654321);
+        assertNotNull(acc);
+        assertEquals("cd", acc.accountType());
+        assertEquals(2.5, acc.getApr());
+        assertEquals(3000, acc.getBalance());
+    }
 
+    @Test
+    public void testDepositToAccount() {
+        processor.process("create checking 12345678 1.0");
+        processor.process("deposit 12345678 500");
+        Account acc = bank.retrieveAccount(12345678);
+        assertEquals(500, acc.getBalance());
+    }
+
+    @Test
+    public void testMultipleDeposits() {
+        processor.process("create checking 12345678 1.0");
+        processor.process("deposit 12345678 200");
+        processor.process("deposit 12345678 300");
+        Account acc = bank.retrieveAccount(12345678);
+        assertEquals(500, acc.getBalance());
+    }
 }
